364a70fb4fb9b94e081fe1b673a9a838229b5f01
KevinZea <kevinstevenzeasuarez@gmail.com>
2023-04-18 10:45:04 -0500

    protect users

--- src/routes/userAdmin.routes.js
+++ src/routes/userAdmin.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createUser, getUsers } from "../controllers/userAdmin.controller.js";
+import {verifyToken} from "../middle/authJwt"
 
 const router = Router()
 
-router.get("/", getUsers)
-router.post("/", createUser)
+router.get("/",verifyToken, getUsers)
+router.post("/",verifyToken, createUser)
 
 export default router
